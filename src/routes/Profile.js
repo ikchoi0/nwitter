84404763be780c5fa429650094227fdcbf65a8b5
Ik Sung Choi <ikchoi0@gmail.com>
2022-08-30 17:06:21 -0400

    add log out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,18 @@
+import { authService } from 'fBase';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
